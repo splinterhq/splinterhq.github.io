decd88ab3eb6de4f0fe9233b67fa4d378f149e86
= <=>
2026-02-26 12:31:18 -0500

    update from website build

--- update.sh
+++ update.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-cp -dpR ../libsplinter/lume-website/_site/* ./
+cp -dpR ../libsplinter/lume-website/_site/* ./ || false
+
+git add .
+git commit -m "update from website build"
+git push
 
 exit $?
 
